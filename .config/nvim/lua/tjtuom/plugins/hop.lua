9fc4ff83a65e083785d0c1394229819cc31d0910
Toni Tuominen <toni@tuominen.co>
2024-08-23 16:02:20 +0300

    Added hop.nvim.

---
+++ .config/nvim/lua/tjtuom/plugins/hop.lua
@@ -0,0 +1,11 @@
+return {
+  "smoka7/hop.nvim",
+  version = "*",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    keys = "etovxqpdygfblzhckisuran",
+  },
+  keys = {
+    { "m", "<cmd>HopWord<cr>", desc = "HopWord" },
+  },
+}
