60f2b9a19af1b996dbb2024e1ddec65a6b895ca2
Toni Tuominen <toni@tuominen.co>
2024-09-11 12:46:20 +0300

    Store oh my posh config locally.

--- .config/fish/conf.d/oh-my-posh.fish
+++ .config/fish/conf.d/oh-my-posh.fish
@@ -1,1 +1,1 @@
-oh-my-posh init fish --config "https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/powerlevel10k_rainbow.omp.json" | source
+oh-my-posh init fish --config "~/.config/fish/themes/powerlevel10k_rainbow.omp.json" | source
