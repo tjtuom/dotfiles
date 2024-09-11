0b405c2fc879f03b1dd4ac621537a0aa8d1e0d91
Toni Tuominen <toni@tuominen.co>
2024-09-11 13:00:33 +0300

    Updated lsp and mason configs.

--- .config/nvim/lua/tjtuom/plugins/lsp/mason.lua
+++ .config/nvim/lua/tjtuom/plugins/lsp/mason.lua
@@ -27,27 +27,25 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
         "html",
         "cssls",
         "tailwindcss",
-        "svelte",
         "lua_ls",
-        "graphql",
         "emmet_ls",
-        "prismals",
+        "ts_ls",
         "pyright",
+        "dockerls",
+        "docker_compose_language_service",
       },
     })
 
     mason_tool_installer.setup({
       ensure_installed = {
         "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
+        "stylua", -- lua formatter,
+        "isort", -- python
+        "black", -- python,
         "pylint",
-        "eslint_d",
       },
     })
   end,
