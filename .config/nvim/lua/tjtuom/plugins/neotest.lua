44dbab6afc42a37913c783256580aae4c676c4c7
Toni Tuominen <toni@tuominen.co>
2024-10-12 23:14:41 +0300

    Added neotest config for neovim.

---
+++ .config/nvim/lua/tjtuom/plugins/neotest.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "jfpedroza/neotest-elixir",
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-elixir"),
+      },
+    })
+  end,
+  keys = {
+    {
+      "<localleader>tt",
+      function()
+        require("neotest").run.run_last()
+      end,
+      desc = "Run last test",
+    },
+    {
+      "<localleader>tr",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run nearest test",
+    },
+    {
+      "<localleader>tf",
+      function()
+        require("neotest").run.run(vim.fn.expand("%"))
+      end,
+      desc = "Run file test",
+    },
+  },
+}
