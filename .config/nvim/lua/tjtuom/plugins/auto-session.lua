5f906476572490ad81133a40f291fc97ef30d1fd
Toni Tuominen <toni@tuominen.co>
2024-10-20 11:24:09 +0300

    Automatic nvim session restore.

--- .config/nvim/lua/tjtuom/plugins/auto-session.lua
+++ .config/nvim/lua/tjtuom/plugins/auto-session.lua
@@ -1,10 +1,12 @@
 return {
   "rmagatti/auto-session",
+  lazy = false,
   config = function()
     local auto_session = require("auto-session")
 
     auto_session.setup({
-      auto_restore_enabled = false,
+      auto_restore = true,
+      auto_save = true,
       auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
     })
 
