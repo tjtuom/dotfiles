2a278ad9c7b82d2837cfe27773595c22e7dd4be1
Toni Tuominen <toni@tuominen.co>
2024-10-21 12:03:41 +0300

    Remove tab keybindings, change buffer close keybinding.

--- .config/nvim/lua/tjtuom/core/keymaps.lua
+++ .config/nvim/lua/tjtuom/core/keymaps.lua
@@ -9,6 +9,7 @@ keymap.set("n", "<esc>", "<cmd>noh<cr><esc>", { silent = true })
 
 keymap.set({ "n", "i" }, "\x03", "<cmd>w<CR>", { desc = "Terminal compatibility to save with cmd-s" })
 keymap.set({ "n", "i" }, "\x04", "<esc><cmd>q<CR>", { desc = "Terminal compatibility to quit with cmd-q" })
+
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
@@ -19,13 +20,7 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
 -- buffer management --
 
 keymap.set("n", "<leader>bb", "<cmd>:b#<CR>", { desc = "Change to last open buffer" })
-keymap.set("n", "<leader>bx", "<cmd>:bdelete<CR>", { desc = "Kill the current buffer" })
+keymap.set("n", "<leader>bd", "<cmd>:bdelete<CR>", { desc = "Kill the current buffer" })
